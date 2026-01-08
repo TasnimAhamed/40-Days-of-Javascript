d2dd89244971421a3e23f6cab645d7d7c48cce9b
TasnimAhamed <tasnim.ahamed.20@gmail.com>
2026-01-08 06:44:46 +0600

    Started 40 Days of Javascript

---
+++ Day 17 - Basic DOM/index.js
@@ -0,0 +1,13 @@
+// DOM Types
+    console.log(document);
+    console.log(document.body);
+    console.log(document.head);
+
+
+// Accessing DOM: Element(tag, attribute, ID, Class)
+
+let headingElem = document.getElementById("heading");
+console.log(headingElem);
+let infoElems = document.getElementsByClassName("info");
+console.log(infoElems);
+
