b6401d85730316ec6a054cfbf099ae86d1af19e4
TasnimAhamed <tasnim.ahamed.20@gmail.com>
2026-01-08 07:17:23 +0600

    Basic element selectors

--- Day 17 - Basic DOM/index.js
+++ Day 17 - Basic DOM/index.js
@@ -6,8 +6,30 @@
 
 // Accessing DOM: Element(tag, attribute, ID, Class)
 
+// ID Selector
 let headingElem = document.getElementById("heading");
 console.log(headingElem);
+
+// Class Selector
 let infoElems = document.getElementsByClassName("info");
 console.log(infoElems);
+console.log(infoElems[0], infoElems[1]);
 
+[...infoElems].forEach((elem, index) => {
+    console.log(index, elem);
+})
+
+// Tag Selector
+let tagElems = document.getElementsByTagName("p");
+console.log(tagElems);
+
+// Query Selector
+let querySelector = document.querySelector(".info");
+console.log(querySelector);
+
+let querySelectorAll = document.querySelectorAll(".info");
+console.log(querySelectorAll);
+
+// Heading using querySelector
+let hElem = document.querySelector("#heading");
+console.log(hElem);
